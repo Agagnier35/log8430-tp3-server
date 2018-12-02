b52689bc9bf702544ebf6eb89cf0410a3950db69
Grago <felix_agagnier@hotmail.ca>
2018-12-02 16:06:54 -0500

    join loses order, so keep order

--- tp3-core/src/main/java/com/log8430/sparkapps/SparkGetMostBoughtProduct.java
+++ tp3-core/src/main/java/com/log8430/sparkapps/SparkGetMostBoughtProduct.java
@@ -1,7 +1,6 @@
 package com.log8430.sparkapps;
 
 import java.net.URL;
-import java.util.*;
 
 import org.apache.spark.api.java.*;
 
@@ -32,16 +31,20 @@ public class SparkGetMostBoughtProduct implements Runnable {
 					.cassandraTable(KEYSPACE_NAME, INVOICE_TABLE_NAME, mapRowTo(InvoiceItem.class));
 			JavaPairRDD<String, Product> productRDD = javaFunctions(sc)
 					.cassandraTable(KEYSPACE_NAME, PRODUCT_TABLE_NAME, mapRowTo(Product.class))
-					.mapToPair(p->new Tuple2<>(p.getProductname(), p));
+					.mapToPair(p -> new Tuple2<>(p.getProductname(), p));
 
-			response = invoiceRDD
-					.mapToPair(i -> new Tuple2<>(i.getProductname(), i.getQuantity()))
-					.reduceByKey((x, y) -> x + y)
-					.mapToPair(Tuple2::swap)
-					.sortByKey(false)
-					.mapToPair(Tuple2::swap)
-					.join(productRDD)
-					.first()._2.swap();
+			Tuple2<String, Integer> topProductName =
+					invoiceRDD
+							.mapToPair(i -> new Tuple2<>(i.getProductname(), i.getQuantity()))
+							.reduceByKey((x, y) -> x + y)
+							.mapToPair(Tuple2::swap)
+							.sortByKey(false)
+							.mapToPair(Tuple2::swap)
+							.first();
+
+			Tuple2<String, Product> topProduct = productRDD.filter(t -> t._1.equals(topProductName._1)).first();
+
+			response = new Tuple2<>(topProduct._2, topProductName._2);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
